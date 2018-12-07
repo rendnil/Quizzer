c8915eb6456842f944ecfe56a8e8ac896f6e3d43
rendnil <andrew.lindner.15@gmail.com>
2018-12-07 17:22:31 -0500

    started grid implementation

--- src/components/SelectionPage/CategoryGrid.js
+++ src/components/SelectionPage/CategoryGrid.js
@@ -1,13 +1,26 @@
 import React from "react"
 import { connect } from 'react-redux'
 
+import {Grid, Container} from 'semantic-ui-react'
+
 
 const CategoryGrid =  ({categories}) => {
   return(
     <React.Fragment>
-      {categories.map((category)=>{
-        return <h1>{category.name}</h1>
-      })}
+      <Grid columns={4}>
+        <Grid.Row>
+        {categories.map((category)=>{
+          return(
+
+            <Grid.Column>
+
+           <h1>{category.name}</h1>
+           </Grid.Column>
+           
+         )
+        })}
+        </Grid.Row>
+      </Grid>
     </React.Fragment>
   )
 }
