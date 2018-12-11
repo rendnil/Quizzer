935db5b265b5f7d3385164162d8425971c9800f1
rendnil <andrew.lindner.15@gmail.com>
2018-12-11 13:24:21 -0500

    added react router infra

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import SelectionPage from "./SelectionPage/SelectionPage"
 import SelectionPage from "./SelectionPage/SelectionPage"
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SelectionPage/>
-
+        <Router>
+          <React.Fragment>
+            <Route exact path = "/" render = {()=> <SelectionPage/>}/>
+            <Route exact path = "/quiz" render = {()=> <QuizPage/>}/>
+          </React.Fragment>
+        </Router>
       </div>
     )
   }
