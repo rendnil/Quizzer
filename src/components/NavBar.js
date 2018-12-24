e2acffc161a3cf2b1a4319ea830d981803047c0b
rendnil <andrew.lindner.15@gmail.com>
2018-12-24 15:25:58 -0500

    update navbar header

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,31 +4,23 @@ import {Menu, Icon, Button} from "semantic-ui-react"
 
 
 const NavBar = (props) => {
-  const navStyle={
-    color:"white"
-  }
   //generate different navbar options and links
   return(
     <Menu inverted borderless color="green">
 
-      {/* display the site name w/icon */}
+      {/* display the site name*/}
 
       {/*<Menu.Item>
         <NavLink  to = "/" style={navStyle}>
           Categories
         </NavLink>
       </Menu.Item> */}
-      <Menu.Item id="navbar-header">
-      {/*<Icon name="question" size="large" style={{ marginRight: '1.5em' }} />*/}
-      <NavLink style={navStyle} to = "/">
-      Quizzer
-      </NavLink>
+      <Menu.Item fitted="vertically" id="navbar-header">
+        {/*<Icon name="question" size="large" style={{ marginRight: '1.5em' }} />*/}
+        <NavLink to = "/">Quizzer</NavLink>
       </Menu.Item>
-
     </Menu>
   )
 }
-
-
 
 export default NavBar
