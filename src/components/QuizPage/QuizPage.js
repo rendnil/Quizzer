3ff1192ea8f1b31c5555986e6903f2ec7a3ab0a0
rendnil <andrew.lindner.15@gmail.com>
2018-12-25 17:07:52 -0500

    refactor quiz page component

--- src/components/QuizPage/QuizPage.js
+++ src/components/QuizPage/QuizPage.js
@@ -1,32 +1,24 @@
 import React from "react"
 import { connect } from 'react-redux'
-import {Segment, Button, Header} from 'semantic-ui-react'
-import {NavLink } from 'react-router-dom';
+import {Button, Header} from 'semantic-ui-react'
 import {Redirect} from "react-router"
 import "../../css/QuizPage.css"
 import QuestionDetail from "./QuestionDetail"
 import {submitAnswer} from "../../redux/actions/submitAnswer"
 
 
-class QuizBox extends React.Component{
+const QuizBox = (props) =>{
 
-  renderBack(){
-    return(
-      <NavLink to = "/">
-        <h1>BACK TO CATEGORIES</h1>
-      </NavLink>
-    )
+
+  const submitAnswer = () => {
+    props.submitAnswer(props.currentQuiz)
   }
 
-  submitAnswer = () => {
-    this.props.submitAnswer(this.props.currentQuiz)
-  }
-
-  btnRender = () => {
-    if (this.props.selectedAnswer){
+  const btnRender = () => {
+    if (props.selectedAnswer){
       return(
         <div id="submit-answer-btn" >
-          <Button  onClick={this.submitAnswer} basic size="large" primary>Submit</Button>
+          <Button  onClick={submitAnswer} basic size="large" primary>Submit</Button>
         </div>
       )
     }else{
@@ -34,36 +26,31 @@ class QuizBox extends React.Component{
     }
   }
 
-  render(){
-    console.log("quiz box render",this.props);
-    if(this.props.currentQuiz&& !this.props.currentQuiz.completed){
-      return(
-        <div className="quiz-page-wrapper">
-          <Header as="h4" id="question-number">{this.props.currentQuiz.questionNumber} of {this.props.currentQuiz.questions.length}</Header>
-          <Header id="quiz-title" as="h1" textAlign='center'>{this.props.currentQuiz.categoryName} Quiz</Header>
+  if(props.currentQuiz&& !props.currentQuiz.completed){
+    return(
+      <div className="quiz-page-wrapper">
+        <Header as="h4" id="question-number">{props.currentQuiz.questionNumber} of {props.currentQuiz.questions.length}</Header>
+        <Header id="quiz-title" as="h1" textAlign='center'>{props.currentQuiz.categoryName} Quiz</Header>
 
-          <QuestionDetail
-          number={this.props.currentQuiz.questionNumber}
-          question={this.props.currentQuiz.questions[this.props.currentQuiz.questionNumber-1]}
-          />
+        <QuestionDetail
+        number={props.currentQuiz.questionNumber}
+        question={props.currentQuiz.questions[props.currentQuiz.questionNumber-1]}
+        />
 
-          {this.btnRender()}
-        </div>
-      )
-    }else{
-      return(<Redirect to ="/results" />)
-    }
+        {btnRender()}
+      </div>
+    )
+  }else{
+    return(<Redirect to ="/results" />)
   }
 }
 
 const mapStateToProps = (state) => {
-  console.log("called map state");
   return{
     currentQuiz: state.currentQuiz,
     selectedAnswer: state.currentQuiz.selectedAnswer
   }
 }
-
 
 const mapDispatchToProps = dispatch => {
   return{
